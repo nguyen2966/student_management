e328ace230b3db11c96d09da47bb4680f4a296e2
luquocphap <phap.luquoc@hcmut.edu.vn>
2026-02-23 11:04:27 +0700

    connect to postgres

--- src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/StudentManagementApplication.java
+++ src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/StudentManagementApplication.java
@@ -1,4 +1,8 @@
 package vn.edu.hcmut.cse.adsoftweng.lab;
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+
+import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudentManagementApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure().systemProperties().load();
+		// ÉP timezone trước khi Spring khởi động
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+
+        System.out.println("JVM Timezone = " + TimeZone.getDefault());
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
