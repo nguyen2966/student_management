4ae5b2475676a98b1ddd8476b6038a444976c0a7
Nguyen <nguyentoankhoa12@gmail.com>
2026-02-23 22:23:36 +0700

    remove redundant code in entry file

--- src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/StudentManagementApplication.java
+++ src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/StudentManagementApplication.java
@@ -4,13 +4,11 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class StudentManagementApplication {
 
 	public static void main(String[] args) {
-		Dotenv.configure().systemProperties().load();
 		// ÉP timezone trước khi Spring khởi động
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 
